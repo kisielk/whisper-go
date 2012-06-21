6de159ee09e7c712d3938248e9c5a0adabfdacfb
Kamil Kisiel <kamil@kamilkisiel.net>
2012-06-21 08:18:44 -0700

    Began implementation of propagate

--- whisper/whisper.go
+++ whisper/whisper.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"io"
 	"os"
+	"time"
 )
 
 type Metadata struct {
@@ -19,6 +20,18 @@ type ArchiveInfo struct {
 	Points          uint32
 }
 
+func (a ArchiveInfo) Retention() uint32 {
+	return a.SecondsPerPoint * a.Points
+}
+
+func (a ArchiveInfo) Size() uint32 {
+	return a.Points * pointSize
+}
+
+func (a ArchiveInfo) End() uint32 {
+	return a.Offset + a.Size()
+}
+
 type Header struct {
 	Metadata Metadata
 	Archives []ArchiveInfo
@@ -26,9 +39,23 @@ type Header struct {
 
 type Archive []Point
 
+
 type Point struct {
 	Timestamp uint32
 	Value     float64
+}
+
+type Whisper struct {
+	Header Header
+	file   *os.File
+}
+
+var pointSize, metadataSize, archiveSize uint32
+
+func init() {
+	pointSize = uint32(binary.Size(Point{}))
+	metadataSize = uint32(binary.Size(Metadata{}))
+	archiveSize = uint32(binary.Size(Archive{}))
 }
 
 func ReadHeader(buf io.ReadSeeker) (header Header, err error) {
@@ -94,8 +121,7 @@ func Create(path string, archives []ArchiveInfo, xFilesFactor float32, aggregati
 		return
 	}
 
-	headerSize := uint32(binary.Size(metadata) + (binary.Size(archives[0]) * len(archives)))
-	pointSize := uint32(binary.Size(Point{}))
+	headerSize := metadataSize + (archiveSize * uint32(len(archives)))
 	archiveOffsetPointer := headerSize
 
 	for _, archive := range archives {
@@ -121,5 +147,152 @@ func Create(path string, archives []ArchiveInfo, xFilesFactor float32, aggregati
 		file.Write(buf[:remaining])
 	}
 
+	return
+}
+
+func Open(path string) (whisper Whisper, err error) {
+	file, err := os.OpenFile(path, os.O_RDWR, 0666)
+	if err != nil {
+		return
+	}
+
+	header, err := ReadHeader(file)
+	if err != nil {
+		return
+	}
+	whisper = Whisper{Header: header, file: file}
+	return
+}
+
+func (w Whisper) Update(point Point) (err error) {
+	now := uint32(time.Now().Unix())
+	diff := now - point.Timestamp
+	if !((diff < w.Header.Metadata.MaxRetention) && diff >= 0) {
+		// TODO: Return an error
+		return
+	}
+
+	var lowerArchives []ArchiveInfo
+	var currentArchive ArchiveInfo
+	for i, currentArchive := range w.Header.Archives {
+		if currentArchive.Retention() < diff {
+			continue
+		}
+		lowerArchives = w.Header.Archives[i+1:]
+	}
+
+	point.Timestamp = point.Timestamp - (point.Timestamp % currentArchive.SecondsPerPoint)
+	_, err = w.file.Seek(int64(currentArchive.Offset), 0)
+	if err != nil {
+		return
+	}
+
+	var basePoint Point
+	err = binary.Read(w.file, binary.BigEndian, basePoint)
+	if err != nil {
+		return
+	}
+
+	if basePoint.Timestamp == 0 {
+		basePoint, err = w.readPoint(currentArchive.Offset)
+		if err != nil {
+			return
+		}
+	} else {
+		myOffset := pointOffset(currentArchive, point.Timestamp, basePoint.Timestamp)
+		err = w.writePoint(myOffset, point)
+		if err != nil {
+			return
+		}
+	}
+
+	higherArchive := currentArchive
+	var lowerArchive ArchiveInfo
+	for _, lowerArchive = range(lowerArchives) {
+		result, e := w.propagate(point.Timestamp, higherArchive, lowerArchive)
+		if ! result {
+			break
+		}
+		if e != nil {
+			err = e
+			return
+		}
+		higherArchive = lowerArchive
+	}
+
+	return
+}
+
+func pointOffset(archive ArchiveInfo, timestamp uint32, baseTimestamp uint32) uint32 {
+	timeDistance := timestamp - baseTimestamp
+	pointDistance := timeDistance / archive.SecondsPerPoint
+	byteDistance := pointDistance * pointSize
+	return archive.Offset + (byteDistance % archive.Size())
+}
+
+func (w Whisper) propagate(timestamp uint32, higher ArchiveInfo, lower ArchiveInfo) (result bool, err error) {
+	lowerIntervalStart := timestamp - (timestamp % lower.SecondsPerPoint)
+
+	basePoint, err := w.readPoint(higher.Offset)
+	if err != nil {
+		return
+	}
+
+	var higherFirstOffset uint32
+	if basePoint.Timestamp == 0 {
+		higherFirstOffset = higher.Offset
+	} else {
+		higherFirstOffset = pointOffset(higher, lowerIntervalStart, basePoint.Timestamp)
+	}
+
+	numHigherPoints := lower.SecondsPerPoint - higher.SecondsPerPoint
+	higherSize := numHigherPoints * pointSize
+	relativeFirstOffset := higherFirstOffset - higher.Offset
+	relativeLastOffset := (relativeFirstOffset + higherSize) % higher.Size()
+	higherLastOffset := relativeLastOffset + higher.Offset
+
+	var higherPoints []Point
+	if higherFirstOffset < higherLastOffset {
+		// The selection is in the middle of the archive. eg: --####---
+		higherPoints = make([]Point, (higherLastOffset - higherFirstOffset) / pointSize)
+		err = w.readPoints(higherFirstOffset, higherPoints)
+		if err != nil {
+			return
+		}
+	} else {
+		// The selection wraps over the end of the archive. eg: ##----###
+		numEndPoints := (higher.End() - higherFirstOffset) / pointSize
+		numBeginPoints := (higherLastOffset - higher.Offset) / pointSize
+		higherPoints = make([]Point, numBeginPoints + numEndPoints)
+
+		err = w.readPoints(higherFirstOffset, higherPoints[:numEndPoints])
+		if err != nil {
+			return
+		}
+		err = w.readPoints(higher.Offset, higherPoints[numEndPoints:])
+		if err != nil {
+			return
+		}
+	}
+
+
+	return
+}
+
+func (w Whisper) readPoint(offset uint32) (point Point, err error){
+	w.file.Seek(int64(offset), 0)
+	err = binary.Read(w.file, binary.BigEndian, point)
+	return
+}
+
+func (w Whisper) readPoints(offset uint32, points []Point) (err error) {
+	w.file.Seek(int64(offset), 0)
+	err = binary.Read(w.file, binary.BigEndian, points)
+	return
+}
+
+func (w Whisper) writePoint(offset uint32, point Point) (err error) {
+	w.file.Seek(int64(offset), 0)
+	err = binary.Write(w.file, binary.BigEndian, point)
 	return
 }
