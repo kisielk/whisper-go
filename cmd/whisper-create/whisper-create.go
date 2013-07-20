e10f2c58b2cdb82c4dfeddde024f13237ebf8f96
Kamil Kisiel <kamil@kamilkisiel.net>
2013-07-19 22:06:21 -0700

    Fix whisper-create

--- cmd/whisper-create/whisper-create.go
+++ cmd/whisper-create/whisper-create.go
@@ -6,19 +6,48 @@ import (
 	"github.com/kisielk/whisper-go/whisper"
 	"log"
 	"os"
+	"strings"
 )
 
-var aggregationMethod whisper.AggregationMethod = whisper.AGGREGATION_AVERAGE
+type aggregationFlag whisper.AggregationMethod
+
+func (f *aggregationFlag) String() string {
+	return f.String()
+}
+
+func (f *aggregationFlag) Set(s string) error {
+	var m whisper.AggregationMethod
+	s = strings.ToLower(s)
+	switch s {
+	case "average":
+		m = whisper.AggregationAverage
+	case "last":
+		m = whisper.AggregationLast
+	case "sum":
+		m = whisper.AggregationSum
+	case "max":
+		m = whisper.AggregationMax
+	case "min":
+		m = whisper.AggregationMin
+	default:
+		m = whisper.AggregationUnknown
+	}
+	*f = aggregationFlag(m)
+	return nil
+}
+
+var aggregationMethod = aggregationFlag(whisper.AggregationAverage)
 var xFilesFactor float64
 
 func main() {
 	flag.Var(&aggregationMethod, "aggregationMethod", "aggregation method to use")
-	flag.Float64Var(&xFilesFactor, "xFilesFactor", 0.5, "x-files factor")
+	flag.Float64Var(&xFilesFactor, "xFilesFactor", whisper.DefaultXFilesFactor, "x-files factor")
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "usage: %s [OPTION]... [FILE] [PRECISION:RETENTION]...\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 	flag.Parse()
+	method := whisper.AggregationMethod(aggregationMethod)
 
 	log.SetFlags(0)
 
@@ -32,7 +61,7 @@ func main() {
 		log.Fatal("error: you must specify at least one PRECISION:RETENTION pair for the archive\n")
 	}
 
-	if aggregationMethod == whisper.AGGREGATION_UNKNOWN {
+	if method == whisper.AggregationUnknown {
 		flag.Usage()
 		log.Fatal(fmt.Sprintf("error: unknown aggregation method \"%v\"", aggregationMethod.String()))
 	}
@@ -50,7 +79,7 @@ func main() {
 		archives = append(archives, archive)
 	}
 
-	_, err := whisper.Create(path, archives, float32(xFilesFactor), aggregationMethod, false)
+	_, err := whisper.Create(path, archives, whisper.CreateOptions{XFilesFactor: float32(xFilesFactor), AggregationMethod: method})
 	if err != nil {
 		log.Fatal(err)
 	}
